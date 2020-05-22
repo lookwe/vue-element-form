a57d5884ded343318dcfae97ed603d1985356402
黄钟飞 <2726768601@qq.com>
2020-05-23 06:44:02 +0800

    设置开发环境/生成环境

--- vue.config.js
+++ vue.config.js
@@ -26,8 +26,8 @@ module.exports = {
 
         //process.env.NODE_ENV 当前运行模式 有3钟模式
         // development = 开发
-        // = 生产环境
-        // = 测试环境
+        // production = 生产环境
+        // test = 测试环境
         if (process.env.NODE_ENV === 'development') {
             config.name =  'Vue 最佳实战开发 开发环境'
         }
@@ -42,8 +42,11 @@ module.exports = {
     // 往常开发出来svg会网上下载下来，为了处理兼容性每次都需要打包，生成好几个不同的字体文件，不同浏览器输出不同字体文件
     // 问题： 如何svg需要改变，又要需要重新下载打包 在放到项目里。变得繁琐不方便
 
-    //解决方案： svg-sprite-loader 组件 安装:npm i svg-sprite-loader -D
+    //解决方案1： svg-sprite-loader 组件 安装:npm i svg-sprite-loader -D
+    // vue-cli-service 主要作用就是帮你把svg打包打一个矢量图库里，每一个svg就是会里面多加一个id为他名称的资源，并且会放在网页顶部
     // 在配置规则时候 可以使用 vue inspect --rule [svg] 规则名
+
+    //方案2:  用线上 的http资源直接饮用
     chainWebpack(config) {
         // 配置svg规则
         // 1. 默认svg的规则不会碰他，得排除 让其他svg规则排除的自己定义目录下svg处理
